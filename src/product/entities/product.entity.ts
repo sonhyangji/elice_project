64e34674d38d2afc0b19d550f9ef0ee9ce1970de
hyangji-son <hyangji.son@hodoolabs.com>
2025-03-15 16:10:51 +0900

    init

---
+++ src/product/entities/product.entity.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Product {
+    @PrimaryGeneratedColumn('uuid')
+    public id: string;
+
+    @Column()
+    public name: string;
+
+    @Column()
+    public description: string;
+
+    @Column()
+    public price: number;
+
+    @Column()
+    public imageUrl: string;
+    
+}
